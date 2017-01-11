d3d8f7561f7b15af8142ce7a9e83b79aac52924c
Robert S. Comer <rscomer@iupui.edu>
2017-01-11 16:29:46 -0500

    N413 example files for "Listings" project

---
+++ roster2.php
@@ -0,0 +1,13 @@
+<?php
+include("php/n413connect.php");
+
+$query = "SELECT id, first_name, last_name, photo from class_roster";
+$result = mysqli_query($link, $query);
+while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
+	echo $row["first_name"].' '. $row["last_name"].' <img src="'.$row["photo"].'" height="30"><br/>';
+}
+
+
+
+
+?>
