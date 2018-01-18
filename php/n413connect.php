10ae94d9ad459b46f440d257a3c88503f9f4dc20
Lefty <brjebrow@umail.iu.edu>
2018-01-18 14:00:39 -0500

    Made changes to the CSS

--- php/n413connect.php
+++ php/n413connect.php
@@ -1,10 +1,26 @@
 <?php
 ////////////////////// MAMP version ///////////////////////
 
+//	$dbhost = 'localhost';
+//	$dbuser = 'root';
+//	$dbpwd  = 'root';
+//	$dbname = 'n413 class_roster';  //'<your MAMP database name>';
+//
+//	$link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
+//
+//	if (!$link) {
+//		die('Connect Error (' . mysqli_connect_errno() . ') '
+//				. mysqli_connect_error());
+//	}
+
+//////////////////////////////////////////////////////////
+
+////////////////////// web-4 method /////////////////////
+
 	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpwd  = 'root';
-	$dbname = 'n413 class_roster';  //'<your MAMP database name>';
+	$dbuser = 'brjebrow';
+	$dbpwd  = 'brjebrow';
+	$dbname = 'brjebrow_db';
 
 	$link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
 
@@ -12,20 +28,4 @@
 		die('Connect Error (' . mysqli_connect_errno() . ') '
 				. mysqli_connect_error());
 	}
-
-//////////////////////////////////////////////////////////
-
-////////////////////// web-4 method /////////////////////
-
-//	$dbhost = 'localhost';
-//	$dbuser = '<your CAS user name>';
-//	$dbpwd  = '<your CAS user name>';
-//	$dbname = '<your CAS user name>_db';
-//	
-//	$link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
-//
-//	if (!$link) {
-//		die('Connect Error (' . mysqli_connect_errno() . ') '
-//				. mysqli_connect_error());
-//	}
 ?>
